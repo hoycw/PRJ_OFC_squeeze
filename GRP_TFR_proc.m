58ddb955b5bb4c4830604d0c5e931e1350fc5fb8
hoycw <hoycw@berkeley.edu>
2022-06-17 20:20:34 -0700

    event locking updated

--- GRP_TFR_proc.m
+++ GRP_TFR_proc.m
@@ -1,8 +1,8 @@
 %% Colin preprocessing script
 % Based on /Analysis/Group/Preprocess/Squeeze_preprocess_all_data_stimulus_locked.m
-clear all
-close all
-clc
+% clear all
+% close all
+% clc
 
 prj_dir = '/Users/colinhoy/Code/PRJ_OFC_squeeze/';
 addpath([prj_dir 'scripts/']);
@@ -74,6 +74,9 @@ for s = 1:4
                     error(['No baseline implemented for bsln_type: ' bsln_types{b_ix}]);
             end
             
+            % Re-align to decision (if needed)
+%             [tfr_r] = fn_realign_tfr_s2r(tfr_s,rt,trial_lim_sec);
+            
             % Trim back down to analysis window
             cfgs = [];
             % cfgs.trials = setdiff([1:numel(trials.trial)], exclude_trials');
@@ -143,3 +146,95 @@ for s = 1:4
 %     load(proc_fname,'sbj_data');
     
 end
+
+    %% Time Frequency analysis on the dataset.
+%     cfg=[];
+%     cfg.trials          = 'all';
+%     cfg.keeptrials      = 'yes';
+%     cfg.output          = 'pow';
+%     cfg.method          = 'mtmconvol';
+%     cfg.taper           = 'hanning';
+%     cfg.foi             = 2:80;
+%     % data.sampleinfo(1,2)
+%     cfg.toi             = -3:0.02:3;
+%     cfg.pad             = 'maxperlen';
+%     % cfg.t_ftimwin    = ones(length(cfg.foi),1).*1;
+%     % Frequecy dependent wavelet length %
+%     cfg.t_ftimwin       = 4./cfg.foi;
+%     freqout             = ft_freqanalysis(cfg, data);
+%     
+%     cfg.baseline     = [-1 0];
+%     cfg.baselinetype = 'zscore';    % 'absolute', 'relative' ratio, 'relchange' %, 'normchange', 'db', 'zscore'.
+%     cfg.parameter    = 'powspctrm';
+%     freqoutbl = ft_freqbaseline(cfg,freqout);
+
+    
+%     databl=data;
+%     % Swap over
+%     databl.trial=databl.trialbl;
+%     databl=rmfield(databl,'trialbl');
+%     
+%     cfg=[];
+%     cfg.trials          = 'all';
+%     cfg.keeptrials      = 'yes';
+%     cfg.output          = 'pow';
+%     cfg.method          = 'mtmconvol';
+%     cfg.taper           = 'hanning';
+%     cfg.foi             =2:80;
+%     % data.sampleinfo(1,2)
+%     cfg.toi             = -3:0.02:3;
+%     cfg.pad             ='maxperlen'
+%     % cfg.t_ftimwin    = ones(length(cfg.foi),1).*1;
+%     % Frequecy dependent wavelet length %
+%     cfg.t_ftimwin       =4 ./cfg.foi;
+%     freqoutblrp             = ft_freqanalysis(cfg, databl);
+    
+    % Manual baseline correction due to the non static baseline when event locking %
+    % Make copy
+%     time=freqoutblrp.time;
+%     st=-1;
+%     ed=0;
+%     [mns Ixst]=min(abs(time-st));
+%     [mne Ixed]=min(abs(time-ed));
+%     
+%     pwrbl=freqoutblrp.powspctrm;
+%     pwrblm=mean(pwrbl(:,:,:,Ixst:Ixed),4);
+%     pwrlbmr=repmat(pwrblm,1,1,1,size(freqout.powspctrm,4));
+    
+    % Baseline correction % Find the baseline %
+    % Note the time axis of the event locked data is different from the
+    % baseline data as I wanted to go back further for the stim locked data %.
+    
+%     pwr=freqout.powspctrm;
+%     pwr=((pwr-pwrlbmr)./pwrlbmr).*100;
+    
+%     freqoutbl=freqout;
+%     freqoutbl.powspctrm=pwr;
+    
+%     %% Plot TFR
+%     if plot_it
+%         %     cfg = [];
+%         %     figure;
+%         %     subplot(2,1,1);
+%         %     cfg.channel      = 'OFC';
+%         %     ft_singleplotTFR(cfg,freqout);
+%         %     xlim([-1.5 1.5]);
+%         %     subplot(2,1,2);
+%         %     cfg.channel      = 'LFP';
+%         %     ft_singleplotTFR(cfg,freqout);
+%         %     xlim([-1.5 1.5]);
+% %         
+% %         cfg = [];
+% %         figure;
+% %         subplot(2,1,1);
+% %         cfg.channel      = 'OFC';
+% %         ft_singleplotTFR(cfg,freqoutbl);
+% %         xlim([-0.5 2]);
+% %         ylim([ 2 35]);
+% %         subplot(2,1,2);
+% %         cfg.channel      = 'LFP';
+% %         ft_singleplotTFR(cfg,freqoutbl);
+% %         xlim([-0.5 2]);
+% %         ylim([ 2 35]);
+%     end
+%     
--- GRP_preprocess.m
+++ GRP_preprocess.m
@@ -11,8 +11,8 @@ SBJs = {'PFC03','PFC04','PFC05','PFC01'}; % 'PMC10'
 sbj_pfc_roi  = {'FPC', 'OFC', 'OFC', 'FPC'};
 
 plot_it  = 0;           % 0/1 whether to creat plots
-evnt_lab = 'dec';      % event to time-lock segmented data {'stim' | 'dec'}
-trl_lim = [-3 6];       % cut trial data (in sec) relative to event
+evnt_lab = 'full';      % event to time-lock segmented data {'stim' | 'dec'}
+trl_lim = [-1.5 2];     % cut trial data (in sec) relative to event
 new_srate = 1000;
 trl_lim_samp = trl_lim.*new_srate;
 n_choice_trl = 75;
@@ -20,6 +20,9 @@ n_choice_trl = 75;
 outlier_std_thresh = 3;
 
 prj_dir = '/Users/colinhoy/Code/PRJ_OFC_squeeze/';
+fig_dir   = [prj_dir 'results/preproc/'];
+if ~exist(fig_dir,'dir'); mkdir(fig_dir); end
+fig_ftype = 'png';
 
 %% Load parameters %%
 % Columns in SqueezeSubjectSyncSummary.xlsx:
@@ -84,10 +87,16 @@ for s=1:4
         if plot_it
             [pxx1,f] = pwelch(nrl_resamp(1,:),new_srate,0,[1:100],new_srate);
             [pxx2,f] = pwelch(nrl_resamp(2,:),new_srate,0,[1:100],new_srate);
+            
+            fig_name = [SBJs{s} '_run_' num2str(r_ix) '_PSDs'];
             figure; loglog(f,pxx1); hold on; loglog(f,pxx2); box off
             xticks([1 4 8 12 20 30:10:100]);
             legend('Signal 1- LFP','Signal 2 - PFC'); legend boxoff
-            title([SBJs{s} ' block ' num2str(r_ix) ' PSDs']);
+            title(fig_name);
+            
+            fig_fname = [fig_dir fig_name '.' fig_ftype];
+            fprintf('Saving %s\n',fig_fname);
+            saveas(gcf,fig_fname);
         end
         
         %% Find and clean up the PCS data
@@ -139,6 +148,7 @@ for s=1:4
         
         % Segment neural data based on event timestamps
         key{r_ix} = nan(size(run_effort{r_ix}));
+        rt{r_ix}  = nan(size(run_effort{r_ix}));
         run_data{r_ix}.time  = cell([n_choice_trl 1]);
         run_data{r_ix}.trial = cell([n_choice_trl 1]);
 %         run_data{r_ix}.sampleinfo = nan([n_choice_trl 2]);
@@ -150,21 +160,28 @@ for s=1:4
             
             % Find event timestamps
             trial_onset = bhv_raw(trl_ix).startStim-PCS_comb_bhv_start;  %1.5 seconds until motor mapping revealed.
-            if bhv_raw(trl_ix).Yestrial ==1
+            if bhv_raw(trl_ix).Yestrial==1
                 choice_onset = bhv_raw(trl_ix).YesChoice-PCS_comb_bhv_start;
             else
                 choice_onset = bhv_raw(trl_ix).NoChoice-PCS_comb_bhv_start;
             end
+            rt{r_ix}(trl_ix) = choice_onset-trial_onset;
             
             % Convert to neural timestamp
-            if strcmp(evnt_lab,'stim')
+            if strcmp(evnt_lab,'full')
                 start_samp = round(trial_onset*new_srate) + trl_lim_samp(1);
-            elseif strcmp(evnt_lab,'dec')
-                start_samp = round(choice_onset*new_srate) + trl_lim_samp(1);
+                end_samp   = round(choice_onset*new_srate) + trl_lim_samp(2);
             else
-                error('not ready for event besides stim or dec');
+                if strcmp(evnt_lab,'stim')
+                    event_samp = round(trial_onset*new_srate);
+                elseif strcmp(evnt_lab,'dec')
+                    event_samp = round(choice_onset*new_srate);
+                else
+                    error('not ready for event besides stim or dec');
+                end
+                start_samp = event_samp + trl_lim_samp(1);
+                end_samp   = event_samp + trl_lim_samp(2);
             end
-            end_samp   = start_samp + trl_lim_samp(2);                         % to end plus 6 seconds after
             
             % Segment neural data
             if all(~isnan([start_samp end_samp])) % skip choice trials with no response
@@ -198,6 +215,7 @@ for s=1:4
     % Combine behavioral data
     bhv = struct;
     bhv.key          = [key{1}; key{2}];
+    bhv.rt           = [rt{1}; rt{2}];
     bhv.blk          = [run_blk_ix{1}; run_blk_ix{2}];
     bhv.blk_trl      = [run_trl_ix{1}; run_trl_ix{2}];
     bhv.run_trl      = [run_all_trl_ix{1}; run_all_trl_ix{2}];
@@ -237,6 +255,23 @@ for s=1:4
                 bhv.(bhv_fields{f})(missing_ix) = [];
             end
         end
+    end
+    
+    %% Plot RTs
+    if plot_it
+        rt_thresh = nanmean(bhv.rt) + nanstd(bhv.rt)*outlier_std_thresh;
+        fig_name = [SBJs{s} '_RT_histogram'];
+        figure('name',fig_name); hold on;
+        histogram(bhv.rt,25);
+        line([nanmean(bhv.rt) nanmean(bhv.rt)],ylim,'Color','k','LineWidth',3);
+        line([rt_thresh rt_thresh],ylim,'Color','r','LineWidth',3);
+        xlabel('Reaction Time (s)');
+        title(SBJs{s});
+        set(gca,'FontSize',16);
+        
+        fig_fname = [fig_dir fig_name '.' fig_ftype];
+        fprintf('Saving %s\n',fig_fname);
+        saveas(gcf,fig_fname);
     end
     
     %% BEHAVIORAL MODELLING %%
@@ -295,6 +330,17 @@ for s=1:4
     % Start by ignoring the autonomic data and just do a correlation of the
     % neural features by the decision features.
     
+    %% Data Visualization for Trial Rejection
+    if plot_it
+        data_plot = rmfield(data,'sampleinfo');
+        
+        cfgp = []; cfgp.viewmode = 'vertical';
+        db_out = ft_databrowser(cfgp, data_plot);
+        
+        cfgr = []; cfgr.method = 'summary';
+        reject_out = ft_rejectvisual(cfgr,data_plot);
+    end
+    
     %% Find and reject neural outliers
     % Get standard deviation and normalize per channel
     trial_std = nan([size(data.trial,1) length(data.label)]);
@@ -310,11 +356,13 @@ for s=1:4
     % subplot(2,1,2); plot(trialmean);
     
     % Find trials with outlier standard deviations
-    reject_ix = [];
+    var_reject_ix = [];
     for ch_ix = 1:2
-        reject_ix = [reject_ix; find(trial_std_norm(:,ch_ix)>outlier_std_thresh)];
+        var_reject_ix = [var_reject_ix; find(trial_std_norm(:,ch_ix)>outlier_std_thresh)];
     end
-    reject_ix = unique(reject_ix);
+    var_reject_ix = unique(var_reject_ix);
+    
+    % Manual rejection based on visual inspection
     if isempty(reject_ix)
         fprintf('%s: No bad trials identified.\n',SBJs{s});
     else
@@ -335,97 +383,6 @@ for s=1:4
         end
     end
     
-    %% Time Frequency analysis on the dataset.
-    cfg=[];
-    cfg.trials          = 'all';
-    cfg.keeptrials      = 'yes';
-    cfg.output          = 'pow';
-    cfg.method          = 'mtmconvol';
-    cfg.taper           = 'hanning';
-    cfg.foi             = 2:80;
-    % data.sampleinfo(1,2)
-    cfg.toi             = -3:0.02:3;
-    cfg.pad             = 'maxperlen';
-    % cfg.t_ftimwin    = ones(length(cfg.foi),1).*1;
-    % Frequecy dependent wavelet length %
-    cfg.t_ftimwin       = 4./cfg.foi;
-    freqout             = ft_freqanalysis(cfg, data);
-    
-    cfg.baseline     = [-1 0];
-    cfg.baselinetype = 'zscore';    % 'absolute', 'relative' ratio, 'relchange' %, 'normchange', 'db', 'zscore'.
-    cfg.parameter    = 'powspctrm';
-    freqoutbl = ft_freqbaseline(cfg,freqout);
-
-    
-%     databl=data;
-%     % Swap over
-%     databl.trial=databl.trialbl;
-%     databl=rmfield(databl,'trialbl');
-%     
-%     cfg=[];
-%     cfg.trials          = 'all';
-%     cfg.keeptrials      = 'yes';
-%     cfg.output          = 'pow';
-%     cfg.method          = 'mtmconvol';
-%     cfg.taper           = 'hanning';
-%     cfg.foi             =2:80;
-%     % data.sampleinfo(1,2)
-%     cfg.toi             = -3:0.02:3;
-%     cfg.pad             ='maxperlen'
-%     % cfg.t_ftimwin    = ones(length(cfg.foi),1).*1;
-%     % Frequecy dependent wavelet length %
-%     cfg.t_ftimwin       =4 ./cfg.foi;
-%     freqoutblrp             = ft_freqanalysis(cfg, databl);
-    
-    % Manual baseline correction due to the non static baseline when event locking %
-    % Make copy
-%     time=freqoutblrp.time;
-%     st=-1;
-%     ed=0;
-%     [mns Ixst]=min(abs(time-st));
-%     [mne Ixed]=min(abs(time-ed));
-%     
-%     pwrbl=freqoutblrp.powspctrm;
-%     pwrblm=mean(pwrbl(:,:,:,Ixst:Ixed),4);
-%     pwrlbmr=repmat(pwrblm,1,1,1,size(freqout.powspctrm,4));
-    
-    % Baseline correction % Find the baseline %
-    % Note the time axis of the event locked data is different from the
-    % baseline data as I wanted to go back further for the stim locked data %.
-    
-%     pwr=freqout.powspctrm;
-%     pwr=((pwr-pwrlbmr)./pwrlbmr).*100;
-    
-%     freqoutbl=freqout;
-%     freqoutbl.powspctrm=pwr;
-    
-    %% Plot TFR
-    if plot_it
-        %     cfg = [];
-        %     figure;
-        %     subplot(2,1,1);
-        %     cfg.channel      = 'OFC';
-        %     ft_singleplotTFR(cfg,freqout);
-        %     xlim([-1.5 1.5]);
-        %     subplot(2,1,2);
-        %     cfg.channel      = 'LFP';
-        %     ft_singleplotTFR(cfg,freqout);
-        %     xlim([-1.5 1.5]);
-        
-        cfg = [];
-        figure;
-        subplot(2,1,1);
-        cfg.channel      = 'OFC';
-        ft_singleplotTFR(cfg,freqoutbl);
-        xlim([-0.5 2]);
-        ylim([ 2 35]);
-        subplot(2,1,2);
-        cfg.channel      = 'LFP';
-        ft_singleplotTFR(cfg,freqoutbl);
-        xlim([-0.5 2]);
-        ylim([ 2 35]);
-    end
-    
     %% SAVE data
     preproc_name = [FileDetails{s,1} '_' evnt_lab]; % Patient code _ event
     sbj_data = struct;
@@ -442,176 +399,4 @@ for s=1:4
     fprintf('Saving %s\n',out_fname);
     save(out_fname,'sbj_data');
     
-    %% Statistical testing %%
-    
-    % Start by examining for a change in the power with the event %
-    
-    % ft_statistics_montecarlo
-    
-    % Start by examining the reward and effort separately %
-    % compute statistics with ft_statfun_indepsamplesregrT
-    
-%     cfg = [];
-%     cfg.channel          = 'OFC';
-%     cfg.statistic        = 'ft_statfun_indepsamplesregrT';
-%     % cfg.statistic        = 'ft_statfun_depsamplesregrT';
-%     cfg.method           = 'montecarlo';
-%     cfg.correctm         = 'cluster';
-%     cfg.numrandomization = 1000;
-%     cfg.alpha            = 0.05;
-%     cfg.tail             = 0;
-%     cfg.correcttail      ='alpha';
-%     cfg.frequency        = [2 30];
-%     % This should be 1 - 3 as this actually include the post presentation
-%     % period only.
-%     cfg.latency          = [0 1.5];
-%     
-%     n1 = size(stake,1);
-%     design(1,1:n1)       = stake';
-%     
-%     cfg.design           = design;
-%     cfg.ivar             = 1;
-%     
-%     statOFCstake = ft_freqstatistics(cfg, freqoutbl);
-%     
-%     cfg = [];
-%     cfg.channel          = 'OFC';
-%     cfg.statistic        = 'ft_statfun_indepsamplesregrT';
-%     % cfg.statistic        = 'ft_statfun_depsamplesregrT';
-%     cfg.method           = 'montecarlo';
-%     cfg.correctm         = 'cluster';
-%     cfg.numrandomization = 1000;
-%     cfg.alpha            = 0.05;
-%     cfg.tail             = 0;
-%     cfg.correcttail      ='alpha';
-%     cfg.frequency        = [2 30];
-%     cfg.latency          = [0 1.5];
-%     
-%     n1 = size(stake,1);
-%     design(1,1:n1)       = EFF(par(2))';
-%     
-%     cfg.design           = design;
-%     cfg.ivar             = 1;
-%     
-%     statOFCeffort = ft_freqstatistics(cfg, freqoutbl);
-%     
-%     cfg = [];
-%     cfg.channel          = 'OFC';
-%     cfg.statistic        = 'ft_statfun_indepsamplesregrT';
-%     % cfg.statistic        = 'ft_statfun_depsamplesregrT';
-%     cfg.method           = 'montecarlo';
-%     cfg.correctm         = 'cluster';
-%     cfg.numrandomization = 1000;
-%     cfg.alpha            = 0.05;
-%     cfg.tail             = 0;
-%     cfg.correcttail      ='alpha';
-%     cfg.frequency        = [2 30];
-%     cfg.latency          = [0 1.5];
-%     
-%     n1 = size(stake,1);
-%     design(1,1:n1)       = SubjVal1';
-%     
-%     cfg.design           = design;
-%     cfg.ivar             = 1;
-%     
-%     statOFCSV = ft_freqstatistics(cfg, freqoutbl);
-%     
-%     cfg = [];
-%     cfg.channel          = 'LFP';
-%     cfg.statistic        = 'ft_statfun_indepsamplesregrT';
-%     cfg.method           = 'montecarlo';
-%     cfg.correctm         = 'cluster';
-%     cfg.numrandomization = 1000;
-%     cfg.alpha            = 0.05;
-%     cfg.tail             = 0;
-%     cfg.correcttail      ='alpha';
-%     cfg.frequency        = [2 30];
-%     cfg.latency          = [0 1.5];
-%     
-%     n1 = size(stake,1);
-%     design(1,1:n1)       = stake';
-%     
-%     cfg.design           = design;
-%     cfg.ivar             = 1;
-%     
-%     statLFPstake = ft_freqstatistics(cfg, freqoutbl);
-%     
-%     cfg = [];
-%     cfg.channel          = 'LFP';
-%     cfg.statistic        = 'ft_statfun_indepsamplesregrT';
-%     cfg.method           = 'montecarlo';
-%     cfg.correctm         = 'cluster';
-%     cfg.numrandomization = 1000;
-%     cfg.alpha            = 0.05;
-%     cfg.tail             = 0;
-%     cfg.correcttail      ='alpha';
-%     cfg.frequency        = [2 30];
-%     cfg.latency          = [0 1.5];
-%     
-%     n1 = size(stake,1);
-%     design(1,1:n1)       = EFF(par(2))';
-%     
-%     cfg.design           = design;
-%     cfg.ivar             = 1;
-%     
-%     statLFPeffort = ft_freqstatistics(cfg, freqoutbl);
-%     
-%     cfg = [];
-%     cfg.channel          = 'LFP';
-%     cfg.statistic        = 'ft_statfun_indepsamplesregrT';
-%     cfg.method           = 'montecarlo';
-%     cfg.correctm         = 'cluster';
-%     cfg.numrandomization = 1000;
-%     cfg.alpha            = 0.05;
-%     cfg.tail             = 0;
-%     cfg.correcttail      ='alpha';
-%     cfg.frequency        = [2 30];
-%     cfg.latency          = [0 1.5];
-%     
-%     n1 = size(stake,1);
-%     design(1,1:n1)       = SubjVal1';
-%     
-%     cfg.design           = design;
-%     cfg.ivar             = 1;
-%     
-%     statLFPSV = ft_freqstatistics(cfg, freqoutbl);
-%     
-%     % PLOT STATS RESULTS
-%     
-%     figure;
-%     subplot(2,3,1);
-%     cfg           = [];
-%     cfg.channel   = {'OFC'};
-%     cfg.parameter = 'stat';
-%     cfg.colormap  = parula;
-%     cfg.ylim      = [2 100];
-%     cfg.xlim      = [-1.5 1.5];
-%     % cfg.zlim      = [ ] ;
-%     cfg.marker    ='off';
-%     cfg.style     = 'fill';
-%     cfg.comment   = 'off';
-%     cfg.maskparameter = 'mask';
-%     cfg.maskstyle = 'outline';
-%     cfg.colorbar  = 'yes';
-%     ft_singleplotTFR(cfg,statOFCstake);
-%     
-%     subplot(2,3,2);
-%     cfg.channel   = {'OFC'};
-%     ft_singleplotTFR(cfg,statOFCeffort);
-%     
-%     subplot(2,3,3);
-%     cfg.channel   = {'OFC'};
-%     ft_singleplotTFR(cfg,statOFCSV);
-%     
-%     subplot(2,3,4);
-%     cfg.channel   = {'LFP'};
-%     ft_singleplotTFR(cfg,statLFPstake);
-%     
-%     subplot(2,3,5);
-%     cfg.channel   = {'LFP'};
-%     ft_singleplotTFR(cfg,statLFPeffort);
-%     
-%     subplot(2,3,6);
-%     cfg.channel   = {'LFP'};
-%     ft_singleplotTFR(cfg,statLFPSV);
 end
